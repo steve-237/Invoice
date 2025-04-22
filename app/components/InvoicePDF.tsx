df2c8ff4535a4bac8096b4a3217b60c92e63cdf8
steve-237 <steve0peg@gmail.com>
2025-04-22 12:53:31 +0200

    feat: download invoice PDF
    - add dependencies to create an invoice image and create PDF with that image
    - add function for downloading and invoice image

--- app/components/InvoicePDF.tsx
+++ app/components/InvoicePDF.tsx
@@ -1,6 +1,9 @@
 import { Invoice, Totals } from "@/type";
+import confetti from "canvas-confetti";
+import html2canvas from "html2canvas-pro";
+import jsPDF from "jspdf";
 import { ArrowDownFromLine, Layers } from "lucide-react";
-import React from "react";
+import React, { useRef } from "react";
 
 interface FacturePDFProps {
   invoice: Invoice;
@@ -18,15 +21,51 @@ function formatDate(dateString: string): string {
 }
 
 const InvoicePDF: React.FC<FacturePDFProps> = ({ invoice, totals }) => {
+  const factureRef = useRef<HTMLDivElement>(null);
+
+  const handleDownloadPdf = async () => {
+    const element = factureRef.current;
+    if (element) {
+      try {
+        const canvas = await html2canvas(element, { scale: 3, useCORS: true });
+        const imgData = canvas.toDataURL("image/png");
+
+        const pdf = new jsPDF({
+          orientation: "portrait",
+          unit: "mm",
+          format: "A4",
+        });
+
+        const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
+
+        pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+        pdf.save(`facture-${invoice.name}.pdf`);
+
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          zIndex: 9999,
+        });
+      } catch (error) {
+        console.error("Erreur lors de la generation du PDF:", error);
+      }
+    }
+  };
+
   return (
     <div className="mt-4 hidden lg:block md:block">
       <div className="border-base-300 border-2 border-dashed rounded-xl p-5">
-        <button className="btn btn-sm btn-accent mb-4">
+        <button
+          onClick={handleDownloadPdf}
+          className="btn btn-sm btn-accent mb-4"
+        >
           Facture PDF
           <ArrowDownFromLine className="w-4" />
         </button>
 
-        <div className="p-8">
+        <div className="p-8" ref={factureRef}>
           <div className="flex justify-between items-center text-sm">
             <div className="flex flex-col">
               <div>
@@ -56,66 +95,67 @@ const InvoicePDF: React.FC<FacturePDFProps> = ({ invoice, totals }) => {
           </div>
 
           <div className="my-6 flex justify-between">
-              <div>
-                <p className="badge badge-ghost mb-2">Emetteur</p>
-                <p className="text-sm font-bold italic">{invoice.issuerName}</p>
-                <p className="text-sm text-gray-500 w-52 break-words">
-                  {invoice.issuerAddress}
-                </p>
-              </div>
-              <div className="text-right">
-                <p className="badge badge-ghost mb-2">Client</p>
-                <p className="text-sm font-bold italic">{invoice.clientName}</p>
-                <p className="text-sm text-gray-500 w-52 break-words">
-                  {invoice.clientAddress}
-                </p>
-              </div>
+            <div>
+              <p className="badge badge-ghost mb-2">Emetteur</p>
+              <p className="text-sm font-bold italic">{invoice.issuerName}</p>
+              <p className="text-sm text-gray-500 w-52 break-words">
+                {invoice.issuerAddress}
+              </p>
             </div>
+            <div className="text-right">
+              <p className="badge badge-ghost mb-2">Client</p>
+              <p className="text-sm font-bold italic">{invoice.clientName}</p>
+              <p className="text-sm text-gray-500 w-52 break-words">
+                {invoice.clientAddress}
+              </p>
+            </div>
+          </div>
 
-            <div className="overflow-x-auto">
-              <table className="table table-zebra">
-                <thead>
-                  <tr>
-                    <th></th>
-                    <th>Description</th>
-                    <th>Quantite</th>
-                    <th>Prix Unitaire</th>
-                    <th>Total</th>
+          <div className="overflow-x-auto">
+            <table className="table table-zebra">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Description</th>
+                  <th>Quantite</th>
+                  <th>Prix Unitaire</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                {invoice.lines.map((line, index) => (
+                  <tr key={index + 1}>
+                    <td>{index + 1}</td>
+                    <td>{line.description}</td>
+                    <td>{line.quantity}</td>
+                    <td>{line.unitPrice.toFixed(2)} $</td>
+                    <td>{(line.quantity * line.unitPrice).toFixed(2)} $</td>
                   </tr>
-                </thead>
-                <tbody>
-                  {invoice.lines.map((line, index) => (
-                    <tr key={index + 1}>
-                      <td>{index + 1}</td>
-                      <td>{line.description}</td>
-                      <td>{line.quantity}</td>
-                      <td>{line.unitPrice.toFixed(2)} $</td>
-                      <td>{(line.quantity * line.unitPrice).toFixed(2)} $</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+          <div className="mt-6 space-y-2 text-md">
+            <div className="flex justify-between">
+              <div className="font-bold">Total Hors Taxes</div>
+              <div>{totals.totalHT.toFixed(2)} $</div>
             </div>
 
-            <div className="mt-6 space-y-2 text-md">
+            {invoice.vatActive && (
               <div className="flex justify-between">
-                <div className="font-bold">Total Hors Taxes</div>
-                <div>{totals.totalHT.toFixed(2)} $</div>
+                <div className="font-bold">TVA {invoice.vatRate} %</div>
+                <div>{totals.totalVAT.toFixed(2)}</div>
               </div>
+            )}
 
-              {invoice.vatActive && (
-                <div className="flex justify-between">
-                  <div className="font-bold">TVA {invoice.vatRate} %</div>
-                  <div>{totals.totalVAT.toFixed(2)}</div>
-                </div>
-              )}
-
-              <div className="flex justify-between">
-                <div className="font-bold">Total Toutes Taxes Comprises</div>
-                <div className="badge badge-accent">{totals.totalHT.toFixed(2)} $</div>
+            <div className="flex justify-between">
+              <div className="font-bold">Total Toutes Taxes Comprises</div>
+              <div className="badge badge-accent">
+                {totals.totalHT.toFixed(2)} $
               </div>
             </div>
-
+          </div>
         </div>
       </div>
     </div>
